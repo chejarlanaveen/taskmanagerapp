d6bd05bb5ce623bca4bdf2535cfd8ad65d9c8c28
chejarlanaveen <chejarlanaveen14@gmail.com>
2024-12-30 11:00:10 +0530

    Update signuppage.js

--- frontend/src/signuppage.js
+++ frontend/src/signuppage.js
@@ -24,7 +24,7 @@ const Signup = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post('https://taskmanagement-nu.vercel.app/signup', { username, password });
+      const response = await axios.post('https://taskmanagerapp-server1.vercel.app/signup', { username, password });
 
       alert(response.data.message);
 
