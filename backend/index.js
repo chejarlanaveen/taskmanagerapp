2322f02719099405b9440ab0818bd17cda1c3a36
chejarlanaveen <chejarlanaveen14@gmail.com>
2024-12-30 10:45:54 +0530

    changes done

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,31 @@
-// const express = require('express');
-// const serverless = require('serverless-http');
-// const path = require('path');
-
-// const app = express();
-
-
-// const server1 = require(path.resolve(__dirname,'server1'));
-// const server2 = require(path.resolve(__dirname,'server2'));
-
-// app.use('/server1',server1);
-// app.use('/server2',server2);
-
-// module.exports = serverless(app);
-// //module.exports = app2;
-
-
 const express = require('express');
-const serverless = require('serverless-http');
+//const serverless = require('serverless-http');
+const path = require('path');
 
 const app = express();
 
 
-app.get('/check',(req,res)=>{
-    try{
-        if(res.status==200){
-            res.status(200).send("server started !!");
-        }
-    }
-    catch(error){
-        res.status(500).send(error.message);
-    }
-});
+const server1 = require(path.resolve(__dirname,'server1/server1.js'));
+const server2 = require(path.resolve(__dirname,'server2'));
 
-module.exports = serverless(app);
+app.use('/server1',server1);
+app.use('/server2',server2);
+
+//module.exports = serverless(app);
+//module.exports = app2;
+
+
+//const serverless = require('serverless-http');
+
+
+
+
+app.get('/check',(req,res)=>{
+
+            res.status(200).send("server started !!");
+        });
+
+//app.listen(5000,console.log("server connected at 5000 port !!"));
+
+//module.exports = serverless(app);
+module.exports=app;
