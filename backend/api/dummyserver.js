d4493dec861f5356470dbd8a4d809c005eb161d6
chejarlanaveen <chejarlanaveen14@gmail.com>
2024-12-30 08:18:20 +0530

    commit all changes

--- backend/api/dummyserver.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/',(req,res)=>{
-    res.send("dummy server");
-});
-
-module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,35 @@
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const path = require('path');
+
+// const app = express();
+
+
+// const server1 = require(path.resolve(__dirname,'server1'));
+// const server2 = require(path.resolve(__dirname,'server2'));
+
+// app.use('/server1',server1);
+// app.use('/server2',server2);
+
+// module.exports = serverless(app);
+// //module.exports = app2;
+
+
 const express = require('express');
 const serverless = require('serverless-http');
-const path = require('path');
 
 const app = express();
 
 
-app.get('/conn',(req,res)=>{
+app.get('/check',(req,res)=>{
     try{
-        if(res.status==200)
-        res.status(200).send("server established !!");
+        if(res.status==200){
+            res.status(200).send("server started !!");
+        }
     }
     catch(error){
         res.status(500).send(error.message);
     }
-    })
-   
-    
-
-app.get('/',(req,res)=>{
-    res.send("dummy server");
 });
 
-
-const server1 = require(path.resolve(__dirname,'server1'));
-const server2 = require(path.resolve(__dirname,'server2'));
-
-app.use('/server1',server1);
-app.use('/server2',server2);
-
 module.exports = serverless(app);
-//module.exports = app2;
